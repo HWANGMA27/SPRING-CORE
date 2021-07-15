a5a0c422f85b09097ca48c9bbfb3db2e3607c0ad
hwangma27 <pikacu22@kakao.com>
2021-07-15 20:41:40 +0900

    [ADD] TEST CLASS > CHECK BEAN CREATION

---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,41 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextInfoTest {
+    
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    
+    @Test
+    @DisplayName("print all bean")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name" + beanDefinitionName + "object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("print application bean")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            /*
+            bean meta data
+            ROLE_APPLICATION : USER REGISTER BEAN
+            ROLE_INFRASTRUCTURE : SPRING INTERNAL USING/CREATE BEAN
+            */
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name" + beanDefinitionName + "object = " + bean);
+            }
+        }
+    }
+}
