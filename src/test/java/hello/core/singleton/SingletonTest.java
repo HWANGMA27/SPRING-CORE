38568cb99986ed0abca76d1191a4a758a1a11d04
hwangma27 <pikacu22@kakao.com>
2021-07-24 23:31:58 +0900

    [ADD] CURRENT ISSUE TEST [ADD] SINGLETON PATTERN TEST

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,35 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("DI container without spring")
+    void pureContainer(){
+        AppConfig appConfig = new AppConfig();
+        //create a new object every time we call
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isNotEqualTo(memberService2);
+    }
+
+    @Test
+    @DisplayName("call object with singleton pattern")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+        //sameAs 객체비교
+        //isEqual 값 비교
+    }
+}
