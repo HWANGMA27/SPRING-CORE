aa98252c53308463c3ba205fda29865fad7c2415
hwangma27 <pikacu22@kakao.com>
2021-08-16 01:04:10 +0900

    [ADD] Bean Scope > Difference between Singleton Scope and Prototype Scope

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,44 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/* prototype bean
+ spring container only involve in creating and initializing bean
+ so after initializing it spring doesn't managing it
+ client should call destroy it self
+ */
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        Assertions.assertThat(prototypeBean1).isNotEqualTo(prototypeBean2);
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct
+        public void init(){
+            System.out.println("Prototype.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("Prototype.destroy");
+        }
+    }
+}
